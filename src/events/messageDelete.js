f4b2ad58cc4c9ac5ac6a484958984aeec9c87524
eNoodles <drikdroidmeeper@gmail.com>
2022-07-11 00:21:18 +0200

    added notification for record deletion

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -1,37 +1,62 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
 const { censored_authors_cache } = require('../managers/censorManager');
-const { cacheDeletedMessage, getJailDataByMessage, deleteRecord, unjailMember } = require('../managers/jailManager');
-const { ids } = require('../utils');
+const { cacheDeletedMessage, getJailDataByMessage, deleteRecord, unjailMember, getRecordsChannel } = require('../managers/jailManager');
+const { ids, colors } = require('../utils');
 
 module.exports = {
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     */
 	async execute(client, message) {
-        // //messageDelete doesn't emit executor data unfortunately, so we have to look it up in the audit log
-        // const audit_logs = await message.guild.fetchAuditLogs({
-        //     type: 72, //MessageDelete
-        //     limit: 1
-        // });
-
-        // //make sure we got the right one by cross referencing audit log entry target and message
-        // const message_delete_entry = audit_logs.entries.find(entry => entry.target.id === message.id);
-        // const deleter = message_delete_entry?.executor ?? message.author;
-
         //potential jail record deletion by user
-        if (message.author.bot && message.channelId === ids.records_ch) {
+        if (message.author?.bot && message.channelId === ids.records_ch) {
             //check if JailData for this message exists
             const data = await getJailDataByMessage(message, message.guild);
             if (data) {
-                //make sure memnber is unjailed
+                //make sure member is unjailed
                 if (!data.record.unjailed) await unjailMember(data).catch(console.error);
+
                 //delete jail record
-                await deleteRecord(data);
-                //no need to cache
+                await deleteRecord(data).catch(console.error);
+
+                //messageDelete doesn't emit executor data unfortunately, so we have to look it up in the audit log
+                const audit_logs = await message.guild.fetchAuditLogs({
+                    type: 72, //MessageDelete
+                    limit: 1
+                });
+
+                //make sure we got the right one:
+                //message deleted mustve been sent by gudbot
+                //mustve been in #criminal-records
+                //current time and deletion time must be within 1000 milliseconds
+                const message_delete_entry = audit_logs.entries.find(entry =>
+                    entry.target.bot && 
+                    entry.target.id === ids.client &&
+                    entry.extra.channel.id === ids.records_ch &&
+                    new Date().getTime() - entry.createdTimestamp < 1000
+                );
+                const deleter = message_delete_entry?.executor; // ?? message.author;
+
+                //if deleter can be determined, send notification
+                if (deleter) {
+                    const embed = new MessageEmbed()
+                        .setDescription(`<@${deleter.id}> deleted <@${data.member.id}>'s jail record from <t:${data.record.jail_timestamp}:f>`)
+                        .setColor(colors.red);
+
+                    await getRecordsChannel().send({ embeds: [embed] });
+                }
+
+                //no need to cache this message
                 return;
             }
         }
 
         //censored messages were originally sent by a user, but message author will be marked as bot
         const censored_message = message.webhookId && censored_authors_cache.get(message.id);
-        //if sent by user, cache message
-        if (!message.author.bot || censored_message)
+        //cache user sent messages
+        //if message is uncached, author will be null
+        if ((message.author && !message.author.bot) || censored_message)
             cacheDeletedMessage(message);
 	}
 };
