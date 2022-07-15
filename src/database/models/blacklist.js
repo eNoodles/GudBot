6cdc4e1f3abdaddb2a0833c91b65ed1860ebd433
eNoodles <drikdroidmeeper@gmail.com>
2022-07-15 16:21:02 +0200

    word must be unique to prevent duplicates

--- src/database/models/blacklist.js
+++ src/database/models/blacklist.js
@@ -1,6 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
 	return sequelize.define('0000_blacklist', {
-        word: DataTypes.STRING,
+        word: {
+            type: DataTypes.STRING,
+            unique: true
+        },
         added_by: DataTypes.STRING
     });
 };
