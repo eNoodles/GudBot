24ea6e289a00a7d9f1fe2f4414a745adac4c51c6
eNoodles <drikdroidmeeper@gmail.com>
2022-07-20 12:59:11 +0200

    bruh

--- src/interactions/selectMenus/starboardSort.js
+++ src/interactions/selectMenus/starboardSort.js
@@ -8,10 +8,11 @@ module.exports = {
     async execute(interaction) {
         //get currently selected sorting option
         const selected_sort_value = interaction.values[0];
+
         //get user and channel ids
-        const select_args = interaction.customId.split('|');
-        const user_id = select_args[1];
-        const channel_id = select_args[2];
+        const args = interaction.customId.split('|');
+        const user_id = args[1];
+        const channel_id = args[2];
         
         await updateStarboardViewer(interaction, {
             user_id: user_id,
