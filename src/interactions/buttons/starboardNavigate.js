c89462e4ffd6e5da7030cf38b5d5105180c05583
eNoodles <drikdroidmeeper@gmail.com>
2022-07-20 12:59:24 +0200

    use getSelectMenuById

--- src/interactions/buttons/starboardNavigate.js
+++ src/interactions/buttons/starboardNavigate.js
@@ -1,5 +1,6 @@
 const { ButtonInteraction } = require('discord.js');
 const { updateStarboardViewer } = require('../../managers/starboardManager');
+const { getSelectMenuById } = require('../../utils');
 
 module.exports = {
     /**
@@ -8,11 +9,10 @@ module.exports = {
 	async execute(interaction) {
         const args = interaction.customId.split('|');
         const entry_id = args[1];
-        const offset = parseInt(args[2], 10) ?? 0;
+        const offset = parseInt(args[2], 10) || 0;
 
         //get select menu
-        const select_menu_row = interaction.message.components?.at(0);
-        const select_menu = select_menu_row?.components?.find(c => c.type === 'SELECT_MENU');
+        const select_menu = getSelectMenuById(interaction.message, 'starboardSort');
         //get currently selected sorting option
         const selected_sort = select_menu?.options.find(option => option.default);
         //get user and channel ids
